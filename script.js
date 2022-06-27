055f061f45937cebf57c116b5ec8a21cbae8f3c0
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-06-27 08:59:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 let weather = {
     apikey: "6faadd1ba633a977a39ab51e3d086152",
     fetchWeather: function (city) {
@@ -23,7 +25,23 @@ let weather = {
            document.querySelector(".temp").innerText = temp + "°C";
            document.querySelector(".humidity").innerText =" Humidity: " + humidity + "%";
            document.querySelector(".wind").innerText =" Wind speed " + speed + " km/h";
+           document.querySelector(".weather").classList.remove("loading");
+           document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+      },
+
+      search: function() {
+           this.fetchWeather(document.querySelector(".search-bar").value);
       }
 };
 
-document.querySelector(".search button").addEventListener("click",)
+document.querySelector(".search button").addEventListener("click", function(){
+     weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+     if(event.key == "Enter"){
+        weather.search();
+     }
+})
+
+weather.fetchWeather("Kolkata");
